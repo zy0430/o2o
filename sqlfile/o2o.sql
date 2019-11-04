6da11296359e89d9343e737c7689d876d8a7ee82
LAPTOP-UDSNSP26\章鱼哥 <648883413@qq.com>
2019-11-05 00:03:57 +0800

    add some entity

--- sqlfile/o2o.sql
+++ sqlfile/o2o.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2019-10-30 23:26:34
+Date: 2019-11-05 00:02:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,6 +31,46 @@ CREATE TABLE `tb_area` (
 
 -- ----------------------------
 -- Records of tb_area
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_head_line
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_head_line`;
+CREATE TABLE `tb_head_line` (
+  `line_id` int(10) NOT NULL AUTO_INCREMENT,
+  `line_name` varchar(1000) NOT NULL,
+  `line_link` varchar(2000) NOT NULL,
+  `line_pic` varchar(2000) NOT NULL,
+  `priority` int(2) DEFAULT 0,
+  `enable_status` int(2) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`line_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_head_line
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_local_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_local_auth`;
+CREATE TABLE `tb_local_auth` (
+  `local_auth_id` int(10) NOT NULL,
+  `user_name` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`local_auth_id`),
+  UNIQUE KEY `uk_local_profile` (`user_name`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `tb_local_auth_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_person_info` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_local_auth
 -- ----------------------------
 
 -- ----------------------------
@@ -57,4 +97,77 @@ CREATE TABLE `tb_person_info` (
 
 -- ----------------------------
 -- Records of tb_person_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_shop
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_shop`;
+CREATE TABLE `tb_shop` (
+  `shop_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '店铺id',
+  `owner_id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `shop_category_id` int(11) NOT NULL,
+  `shop_name` varchar(255) DEFAULT NULL,
+  `shop_desc` varchar(2000) DEFAULT NULL,
+  `shop_addr` varchar(2000) DEFAULT NULL,
+  `phone` varchar(128) DEFAULT NULL,
+  `shop_img` varchar(2000) DEFAULT NULL,
+  `priority` int(2) NOT NULL DEFAULT 0,
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  `enable_status` int(2) NOT NULL DEFAULT 0,
+  `advice` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`shop_id`),
+  KEY `area_id` (`area_id`),
+  KEY `shop_category_id` (`shop_category_id`),
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `tb_shop_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `tb_area` (`area_id`),
+  CONSTRAINT `tb_shop_ibfk_2` FOREIGN KEY (`shop_category_id`) REFERENCES `tb_shop_category` (`shop_category_id`),
+  CONSTRAINT `tb_shop_ibfk_3` FOREIGN KEY (`owner_id`) REFERENCES `tb_person_info` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_shop
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_shop_category
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_shop_category`;
+CREATE TABLE `tb_shop_category` (
+  `shop_category_id` int(10) NOT NULL AUTO_INCREMENT,
+  `shop_category_name` varchar(255) NOT NULL,
+  `shop_category_desc` varchar(1000) DEFAULT NULL,
+  `shop_category_img` varchar(2000) NOT NULL,
+  `priority` int(2) NOT NULL DEFAULT 0,
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  `parent_id` int(2) NOT NULL,
+  PRIMARY KEY (`shop_category_id`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `tb_shop_category_ibfk_1` FOREIGN KEY (`shop_category_id`) REFERENCES `tb_shop_category` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_shop_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_wechat_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_wechat_auth`;
+CREATE TABLE `tb_wechat_auth` (
+  `wechat_auth_id` int(10) NOT NULL COMMENT '微信号',
+  `user_id` int(10) NOT NULL COMMENT '用户id',
+  `open_id` varchar(1024) NOT NULL,
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`wechat_auth_id`),
+  UNIQUE KEY `open_id` (`open_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `tb_wechat_auth_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_person_info` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_wechat_auth
 -- ----------------------------
